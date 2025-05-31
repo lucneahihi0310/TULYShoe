f5bc31448a8aedbd73ad9c80f288cf172bd63914
Thangtd0202 <thangtdhe212@gmail.com>
2025-05-31 09:17:11 +0700

    Create footder.jsx
    eheehehehhe

---
+++ tuly-shose-frontend/src/Components/Footder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footder = () => {
+  return (
+    <div>
+        <h1>hehehehhe shoe</h1>
+        <h1>tu ehhehehe ly</h1>
+    </div>
+  )
+}
+
+export default Footder
