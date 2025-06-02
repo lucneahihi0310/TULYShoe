2cea4d6c336f410ed0e3e4b17785c99cbe3b34f8
Thangtd0202 <thangtdhe212@gmail.com>
2025-06-02 14:20:40 +0700

    Update ManagerDashboard

--- tuly-shose-frontend/src/Components/ManagerDashboard.jsx
+++ tuly-shose-frontend/src/Components/ManagerDashboard.jsx
@@ -1,71 +1,74 @@
 import React from "react";
-import { Col, Input, Row, Button, Card } from "antd";
+import { Col, Input, Row, Button, Card, Space, Divider } from "antd";
 const { Meta } = Card
+import { SearchOutlined, PlusOutlined } from '@ant-design/icons'
 
 const ManagerDashboard = () => {
     return (
         <div>
-            <Row justify={'center'} align={'middle'}>
-                <Col span={8}>
-                    <div>products</div>
+            <Row gutter={16} style={{padding: '10px'}}>
+                <Col span={4}>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <h2>Products</h2>
+                    </div>
                 </Col>
-                <Col span={8}>
-                    <Input placeholder="Basic usage" />
+                <Col span={8} offset={4}>
+                    <Input placeholder="Search product..." prefix={<SearchOutlined />} />
                 </Col>
-                <Col span={8}>
-                    <Button ghost>
-                        Add new product
+                <Col span={4} offset={4}>
+                    <Button style={{ color: 'black' }} shape="round" icon={<PlusOutlined />}>
+                        Add New Product
+                    </Button>
+                </Col>
+            </Row>
+            <Row justify={'center'} align={'middle'} style={{padding: '10px'}}>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
+                    </Button>
+                </Col>
+                <Col style={{ padding: '5px' }}>
+                    <Button type="text" block>
+                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
                     </Button>
                 </Col>
             </Row>
             <Row justify={'center'} align={'middle'}>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-                <Col style={{ padding: '5px' }}>
-                    <Button type="text" block>
-                        <h4 style={{ color: "#d0d9db" }}>All products</h4>
-                    </Button>
-                </Col>
-            </Row>
-            <Row>
                 <div style={{ margin: '20px' }}>
                     <Card
                         hoverable
