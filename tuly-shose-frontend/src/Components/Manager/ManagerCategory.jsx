77f501e32832c6489fbb1363fcc77261363f3330
Thangtd0202 <thangtdhe212@gmail.com>
2025-06-04 23:08:54 +0700

    update managercategory and create manageraccount

--- tuly-shose-frontend/src/Components/Manager/ManagerCategory.jsx
+++ tuly-shose-frontend/src/Components/Manager/ManagerCategory.jsx
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Col, Input, Row, Button, Card, Space, Modal, Form, Table } from "antd";
+const { Meta } = Card
+import { SearchOutlined, PlusOutlined } from '@ant-design/icons'
 
 const ManagerCategory = () => {
-
+    //xử lý add category
+    const [addCategory, setAddCategory] = useState(false);
+    const showAddCategoryModal = () => {
+        setAddCategory(true);
+    };
+    const handleAddCategoryCancel = () => {
+        setAddCategory(false);
+    };
     return (
-        <>
-            <h1>hhh</h1>
-        </>
+        <div style={{ borderRadius: '20px', padding: '10px', backgroundColor: '#f7f9fa' }}>
+            <Row gutter={16} style={{ padding: '10px' }}>
+                <Col span={4}>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <h4>Categories</h4>
+                    </div>
+                </Col>
+                <Col span={8} offset={4}>
+                    <Input placeholder="Search category..." prefix={<SearchOutlined />} />
+                </Col>
+                <Col span={4} offset={4}>
+                    <Button style={{ color: 'black' }} shape="round" icon={<PlusOutlined />} onClick={showAddCategoryModal}>
+                        Add New Category
+                    </Button>
+                    <Modal
+                        title="Add new category"
+                        closable={{ 'aria-label': 'Custom Close Button' }}
+                        open={addCategory}
+                        onCancel={handleAddCategoryCancel}
+                        footer={null}>
+                        <Form
+                            name="wrap"
+                            labelCol={{ flex: '110px' }}
+                            labelAlign="left"
+                            labelWrap
+                            wrapperCol={{ flex: 1 }}
+                            colon={false}
+                            style={{ maxWidth: 600 }}
+                        >
+                            <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item label="Description" name="description">
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item label=" ">
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Modal>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    a
+                </Col>
+            </Row>
+        </div>
     );
 };
 
