fc693e532ba002e3de7857541e31f5cd35b0c180
Thangtd0202 <thangtdhe212@gmail.com>
2025-07-05 17:23:23 +0700

    import apiservice to managersize.jsx

--- tuly-shose-frontend/src/Components/Manager/ManagerSize.jsx
+++ tuly-shose-frontend/src/Components/Manager/ManagerSize.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Col, Input, Row, Button, Space, Modal, Form, Table, Select, Tag, Popconfirm } from "antd";
 import { SearchOutlined, PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import axios from 'axios';
+import { fetchData, postData, updateData, deleteData } from "../API/ApiService";
 
 const ManagerSize = () => {
     const [categories, setCategories] = useState([]);
