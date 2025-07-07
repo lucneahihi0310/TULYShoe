742171f4fa6b37e611ad3bb17ca50d3bd67a6f60
Thangtd0202 <thangtdhe212@gmail.com>
2025-07-07 15:18:20 +0700

    update product detail controller

--- tuly-shose-backend/controllers/product_detail.controller.js
+++ tuly-shose-backend/controllers/product_detail.controller.js
@@ -13,22 +13,24 @@ const formatDateTime = (date) => {
 
 exports.list_product_detail = async (req, res, next) => {
     try {
-        const newProductDetail = await product_detail.find();
-        // const listProduct = newProduct.map((p) => {
-        //     return {
-        //         _id: p.id,
-        //         productName: p.productName,
-        //         description: p.description,
-        //         price: p.price,
-        //         categories_id: p.categories_id,
-        //         brand_id: p.brand_id,
-        //         material_id: p.material_id,
-        //         form_id: p.form_id,
-        //         create_at: formatDateTime(p.create_at),
-        //         update_at: formatDateTime(p.update_at)
-        //     }
-        // })
-        res.status(201).json(newProductDetail);
+        const newProductDetail = await product_detail.find().populate("product_id").populate("color_id").populate("size_id").populate("discount_id").populate("product_detail_status");
+        const listProductDetail = newProductDetail.map((p) => {
+            return {
+                _id: p.id,
+                product_id: p.product_id,
+                color_id: p.color_id,
+                size_id: p.size_id,
+                discount_id: p.discount_id,
+                inventory_numer: p.inventory_numer,
+                sold_number: p.sold_number,
+                price_after_discount: p.price_after_discount,
+                images: p.images,
+                product_detail_status: p.product_detail_status,
+                create_at: formatDateTime(p.create_at),
+                update_at: formatDateTime(p.update_at)
+            }
+        })
+        res.status(201).json(listProductDetail);
     } catch (error) {
         next(error);
     }
