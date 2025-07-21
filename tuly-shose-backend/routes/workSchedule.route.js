fe7515cc11bec0e6d6c77e1668f720a8058fbc3d
Thangtd0202 <thangtdhe212@gmail.com>
2025-07-22 01:25:54 +0700

    update add work schedule

--- tuly-shose-backend/routes/workSchedule.route.js
+++ tuly-shose-backend/routes/workSchedule.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getSchedulesByStaff, checkIn, checkOut } = require('../controllers/workSchedule.controller');
+const { getSchedulesByStaff, checkIn, checkOut, createSchedule, updateSchedule, deleteSchedule } = require('../controllers/workSchedule.controller');
 const router = express.Router();
 
 // Route lấy lịch làm việc của 1 staff
 router.get('/:staffId', getSchedulesByStaff);
 router.put('/checkin/:scheduleId', checkIn);
 router.put('/checkout/:scheduleId', checkOut);
+
+router.post('/', createSchedule);
+router.put('/:id', updateSchedule);
+router.delete('/:id', deleteSchedule);
 module.exports = router;
